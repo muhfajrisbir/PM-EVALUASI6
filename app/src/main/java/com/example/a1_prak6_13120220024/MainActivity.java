ede9381590c6db1f8acdb8aba95f18e591b678e6
aisananta <kolopuha415@gmail.com>
2024-05-21 10:58:36 +0800

    Update MainActivity dan TampilDataActivity

--- app/src/main/java/com/example/a1_prak6_13120220024/MainActivity.java
+++ app/src/main/java/com/example/a1_prak6_13120220024/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a1_prak6_13120220024;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtSTb, txtNama, txtAngkatan;
+    private RestHelper restHelper;
+    private Mahasiswa mhs;
+    private Intent intentEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        restHelper = new RestHelper(this);
+        intentEdit = null;
+
+        txtSTb = findViewById(R.id.txt_stb);
+        txtNama = findViewById(R.id.txt_nama);
+        txtAngkatan = findViewById(R.id.txt_angkatan);
+    }
+
+    private void clearData (){
+        txtSTb.setText("");
+        txtNama.setText("");
+        txtAngkatan.setText("");
+        intentEdit = null;
+        txtSTb.requestFocus();
+    }
+
+    private void btnSimpanClick(View view) {
+        mhs = new Mahasiswa(
+                txtSTb.getText().toString(),
+                txtNama.getText().toString(),
+                Integer.parseInt(txtAngkatan.getText().toString())
+        );
+        try {
+            if (intentEdit == null)
+                restHelper.insertData(mhs.toJSON());
+            else
+                restHelper.editData(intentEdit.getStringExtra("stb"), new Mahasiswa(
+                        txtSTb.getText().toString(),
+                        txtNama.getText().toString(),
+                        Integer.parseInt(txtAngkatan.getText().toString())
+                ));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        clearData();
     }
 }
